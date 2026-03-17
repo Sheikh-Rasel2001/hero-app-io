0c06cb9608ffb096c05a869314330871d9e7307f
Sheikh-Rasel2001 <mhrasel191132@gmail.com>
2026-03-17 11:55:01 +0600

    error page design

--- src/Pages/Apps/Apps.jsx
+++ src/Pages/Apps/Apps.jsx
@@ -1,11 +1,19 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import { CiSearch } from "react-icons/ci";
 import AppCard from './AppCard/AppCard';
 
 const Apps = ({promiseApps}) => {
+    const [search, setSearch] = useState('');
     const allApps = use(promiseApps);
+    const searchApps = allApps.filter(app => app.title.toLowerCase().includes(search.toLowerCase()));
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        const search = e.target.value;
+        setSearch(search);
+    }
     return (
-        <div className='max-w-6xl mx-auto'>
+        <div className='max-w-6xl mx-auto px-4'>
             <div className='text-center my-8'>
                 <h1 className='text-4xl font-bold mb-2 text-[#001931]'>Our All Applications</h1>
                 <p className='text-sm text-gray-400'>Explore All Apps on the Market developed by us. We code for Millions</p>
@@ -16,14 +24,20 @@ const Apps = ({promiseApps}) => {
                     <p>({allApps.length}) App Found</p>
                 </div>
                 <div className='relative'>
-                    <input type="search" name="search" placeholder='Search Apps' className='border rounded-lg py-1 pl-8' />
+                    <input 
+                    onChange={handleSearch}
+                    type="search"
+                    name="search" 
+                    value={search}
+                    placeholder='Search Apps' 
+                    className='border rounded-lg py-1 pl-8' />
                     <CiSearch className='absolute top-2 left-3' />
                 </div>
             </div>
             {/* all apps */}
-            <div className='grid grid-cols-1 lg:grid-cols-4 gap-4 px-4 mb-10'>
+            <div className='grid grid-cols-1 lg:grid-cols-4 gap-4 mb-10'>
                 {
-                    allApps.map(app => <AppCard key={app.id} app={app}></AppCard>)
+                    searchApps.map(app => <AppCard key={app.id} app={app}></AppCard>)
                 }
             </div>
         </div>
--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import errorImage from '../../assets/error-404.png';
+import { Link } from 'react-router';
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h1>404 not found</h1>
+        <div className='max-w-6xl mx-auto'>
+            <div className='py-10 w-2/3 mx-auto'>
+                <div className='w-96 mx-auto'>
+                    <img src={errorImage} alt="Error Page" className='w-full'/>
+                </div>
+                <div className='mt-10 text-center space-y-2'>
+                    <h1 className='text-4xl text-[#001931] font-bold'>Oops, page not found!</h1>
+                    <p className='text-gray-400'>The page you are looking for is not available.</p>
+                    <Link to='/'>
+                        <button className='btn-bg px-5 py-2 text-white font-semibold rounded-lg cursor-pointer'>Go Back</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
